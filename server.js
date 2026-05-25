775da7f087168b670d8cd4d32987d8714c86ab0c
Jivika V S <vsjivika2005@gmail.com>
2026-05-25 14:05:47 +0530

    Auto run main.js on Railway

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const { exec } = require('child_process');
 
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('Python Quiz Automation Running');
+});
+
+exec('node main.js', (error, stdout, stderr) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+
+  console.log(stdout);
 });
 
 const PORT = process.env.PORT || 3000;
